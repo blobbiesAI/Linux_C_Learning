d79cecc84886fd5e44fbb42c5b9e6318ef16d9a9
blobbiesAI <blobbiesAI@icloud.com>
2021-10-15 23:37:36 -0700

    modify dir.c

--- dir.c
+++ dir.c
@@ -4,26 +4,42 @@
 #include <stdlib.h>
 #include <errno.h>
 
-int main(int argc, char **argv){
-	DIR* dirptr = opendir(argv[1]);
+int printfdir(DIR* dirptr){
 	if(dirptr==NULL){
 here:
-		perror("ERROR:open a dir wrong");
+		perror("error:open a dir wrong");
 		return -1;
 	}
 
 	struct dirent * dir;
 	errno = 0;// man readdir; man errno
-        while((dir = readdir(dirptr))!=NULL){
+       	while((dir = readdir(dirptr))!=NULL){
 		if(errno) goto here; 
-		printf("Inode:%ld ", dir->d_ino);
+		printf("inode:%ld ", dir->d_ino);
 		printf("file type:%d ", dir->d_type);
 		printf("file name:%s\n", dir->d_name);
 	}
 	int dirclose = closedir(dirptr);
 	if(dirclose==-1){
-		perror("ERROR:close a dir wrong");
+		perror("error:close a dir wrong");
 		return -1;
+	}
+	printf("\n");
+	
+}
+
+int main(int argc, char **argv){
+	DIR* dirptr;
+
+	if(argc==1){//default current dir
+		dirptr = opendir(".");
+		printfdir(dirptr);
+	}
+	else{
+		for(int i=1; i<argc; i++){
+			dirptr = opendir(argv[i]);
+			printfdir(dirptr);
+		}
 	}
 
 	return 0;
