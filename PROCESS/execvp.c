aa6353dec92b9f14e4946bc095c03d7f43ab0363
blobbiesAI <blobbiesAI@icloud.com>
2021-10-19 05:03:28 -0700

    fork-execvp.c

--- PROCESS/execvp.c
+++ PROCESS/execvp.c
@@ -1,17 +1,34 @@
 #include <unistd.h>
 #include <stdio.h>
-
-//#define FORK_CHILD_PROCESS
+#include <stdlib.h>
+#define FORK_CHILD_PROCESS
 
 int main(int argc, char **argv){
 	printf("before execvp......\n");
 #ifdef FORK_CHILD_PROCESS
 	int pid = fork();
+	if(pid==-1){
+		perror("ERROR:create child process wrong");
+		return -1;
+	}
 	printf("pid = %d\n", pid);
 #endif
-	
-	int state = execvp(argv[1], );
-
+#if 0
+	char *args[5];
+	args[0] = "ls";
+	args[1] = "-l";
+	args[2] = "-a";
+	args[3] = NULL;
+	int state = execvp(args[0], args);
+#endif	
+	if(pid==0){
+		int state = execvp(argv[1], argv+1);
+		if(state==-1){
+			perror("ERROR:execvp wrong");
+			return -1;
+		}
+		exit(0);
+	}
 	printf("after execvp......\n");
 
 	return 0;
--- PROCESS/fork.c
+++ PROCESS/fork.c
@@ -1,12 +1,14 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(void){
 	printf("hello\n");
 	int proid = fork();
+	printf("id: %d\n", getpid());
 	printf("world\n");
 	printf("pid: %d\n", proid);
+	exit(0);// exit current process;
 	if(proid==0){
 		printf("running child process\n");
 	}
