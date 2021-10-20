4424ce1c03936e4ee425da1ccfb7e79f2548c034
blobbiesAI <blobbiesAI@icloud.com>
2021-10-20 11:16:33 -0700

    add signal.c

---
+++ PROCESS/signal.c
@@ -0,0 +1,35 @@
+#include <signal.h>
+#include <stdio.h>
+
+
+//kill -l
+
+typedef void (*sighandler_t)(int);
+
+struct signal_class{
+	int signum;
+	sighandler_t handler;
+};
+
+void add(int a){
+	printf("%d\n",a);
+	return;
+}
+
+void sub(int a){
+	printf("%d\n",a);
+	return;
+}
+
+
+int main(void){
+	struct signal_class handler;
+	handler.signum = SIGINT;handler.handler = add;
+	signal(handler.signum, handler.handler);
+
+	handler.signum = SIGHUP;handler.handler = add;
+	signal(handler.signum, handler.handler);
+
+	while(1);
+	return 0;
+}
