65d317db4c94a83be3689406e3720a11e04eba76
blobbiesAI <blobbiesAI@icloud.com>
2021-10-23 08:14:38 -0700

    modify minishell.c

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -3,11 +3,83 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
+#include <string.h>
 #define ARGVLEN 32
 #define STRLEN 128
 
+
+void read_analyse_cmd(char *str, char *args[]){
+	//char str[STRLEN] = {0};  //ls\0-a\0-l\0
+	int i = 0;int j = 0;
+	char *p = str;
+	char c = 0;
+	while(1){	
+			
+		c = getchar();
+		if(c==' ' || c=='\t' || c=='\n'){
+			if(i==0){
+				if(c=='\n') 
+					break;  //exit when we have kong ge after last variable
+				continue;
+			}
+			else{
+				p[i++]='\0';
+				args[j++] = p;
+				p +=i;
+			       	i=0;
+				if(c=='\n')
+					break; //exit when we donot have kong ge after last variable	
+			}
+		}
+		else{
+			p[i++] = c;
+		}
+			
+	}
+	return;	
+}
+
+
+void run_cmd(char *argv[]){
+	int p_stat = vfork();
+	if(p_stat == 0){
+		if(execvp(argv[0],argv) == -1){
+			perror("execvp");
+			_exit(-1);
+		}
+		_exit(0);
+			
+	}
+	else if(p_stat > 0){
+		wait(NULL);
+	}
+	else{
+		perror("fork");
+		exit(-1);
+	}
+	return;
+}
+
 int main(void){
+	char str[STRLEN] ={0}; 
+	char *args[ARGVLEN] = {NULL};
+	while(1){
+		read_analyse_cmd(str,args);
+		run_cmd(args);
+		memset(str,0,sizeof(char)*STRLEN);
+		memset(args,0,sizeof(char*)*ARGVLEN);
+	}
+	return 0;
+}
+
+	
+
+
+
+
+
+/*
+int main1(void){
 	char *argv[ARGVLEN] = {NULL};
 	char str[STRLEN] = {0};  //ls\0-a\0-l\0
 	int i = 0;int j = 0;
@@ -54,3 +126,5 @@ int main(void){
 	}
 	return 0;
 }
+
+*/
